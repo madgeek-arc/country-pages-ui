c3123f4a7a19a8649355a9b9154940c8fffdd214
jBalasis <j.balasis92@gmail.com>
2025-10-30 15:44:32 +0200

    build: Upgrade angular to v18, removed unused guards

--- src/app/pages/services/http-interceptor.service.ts
+++ src/app/pages/services/http-interceptor.service.ts
@@ -1,9 +1,9 @@
-import {Injectable} from "@angular/core";
-import {HttpEvent, HttpHandler, HttpInterceptor, HttpRequest, HttpErrorResponse} from "@angular/common/http";
-import {Observable, throwError} from "rxjs";
-import {catchError} from "rxjs/operators";
-import {Router} from "@angular/router";
-import {AuthenticationService} from "../../../survey-tool/app/services/authentication.service";
+import { Injectable } from "@angular/core";
+import { HttpErrorResponse, HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from "@angular/common/http";
+import { Observable, throwError } from "rxjs";
+import { catchError } from "rxjs/operators";
+import { Router } from "@angular/router";
+import { AuthenticationService } from "../../../survey-tool/app/services/authentication.service";
 
 @Injectable()
 export class HttpInterceptorService implements HttpInterceptor {
